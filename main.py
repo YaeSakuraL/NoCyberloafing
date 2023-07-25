e7bb01f12c17c0a6489499899c439ec75baf3f12
YaeSakuraL <yaesakura@qq.com>
2023-07-26 01:10:36 +0800

    Add: multichoice&cancel choice

--- main.py
+++ main.py
@@ -8,6 +8,47 @@ from glob import glob
 import random
 import threading
 
+def init():
+    pygame.mixer.init()
+    os.system('cls')
+    print('Welcome to Cyberloafing Detector~♪')
+    print('Max cyberloafing time(seconds)~♪:', end=' ')
+    global max_cyberloafing_time
+    max_cyberloafing_time = int(input()) * 10
+    print('Max working time(seconds)~♪:', end=' ')
+    global max_working_time
+    max_working_time = int(input()) * 10
+
+    global focus_window
+    window_list = get_windows()
+    focus_window = []
+
+    os.system('cls')
+    for i in range(len(window_list)):
+        print(i, window_list[i].title)
+    print('Select the window you want to focus on~♪')
+    index = int(input())
+    focus_window.append(window_list[index])
+    while True:
+        os.system('cls')
+        for i in range(len(window_list)):
+            if window_list[i] in focus_window:
+                #print in purple
+                print('\033[35m', end='')
+                print(">", i, window_list[i].title)
+                print('\033[0m', end='')
+            else:
+                print(i, window_list[i].title)
+        print('Select the window you want to focus on, press 999 to finish selecting~♪')
+        index = int(input())  
+        if index == 999:
+            break
+
+        if window_list[index] in focus_window:
+            focus_window.remove(window_list[index])
+        else:
+            focus_window.append(window_list[index])
+
 def get_windows():
     windows = []
     for x in pyautogui.getAllWindows():  
@@ -24,9 +65,16 @@ def check_foreground():
     cyberloafing_voice = glob('./voice/Cyberloafing/*.wav')
     working_count = 0
     cyberloafing_count = 0
+    cyberloafing_flag = False
     while True:
         time.sleep(0.1)
-        if focus_window.isActive:
+        for window in focus_window:
+            if window.isActive:
+                cyberloafing_flag = False
+                break
+            else:
+                cyberloafing_flag = True
+        if cyberloafing_flag:
             cyberloafing_count = 0
             working_count += 1
             print('Working hard~♪',end='\r')
@@ -66,22 +114,10 @@ def exit():
             break
 
 if __name__ == '__main__':
-    pygame.mixer.init()
-    os.system('cls')
-    print('Welcome to Cyberloafing Detector~♪')
-    print('Max cyberloafing time(seconds)~♪:', end=' ')
-    max_cyberloafing_time = int(input()) * 10
-    print('Max working time(seconds)~♪:', end=' ')
-    max_working_time = int(input()) * 10
+    init()
 
-    window_list = get_windows()
-    for i in range(len(window_list)):
-        print(i, window_list[i].title)
-    print('Select the window you want to focus on~♪')
-    index = int(input())
-    focus_window = window_list[index]
     os.system('cls')
-    print('Focus on the window:', focus_window.title, '~♪')
+    print('Start focusing~♪')
     print('Press \'Ecs\' to exit~♪')
 
     stop_threads = False
